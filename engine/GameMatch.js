c0a6f459e8c76ff8b09e412e72f5c17494aa828a
ricci1mmm <76752981+ricci1mmm@users.noreply.github.com>
2026-07-28 00:10:14 +0300

    Add formation based player positioning to GameMatch

--- engine/GameMatch.js
+++ engine/GameMatch.js
@@ -7,6 +7,8 @@ import {CoachAI} from './coachAI.js';
 import {StaminaSystem} from './stamina.js';
 import {Referee} from './referee.js';
 import {BallAI} from './ballAI.js';
+import {PositioningEngine} from './positioning.js';
+import {createPositions} from './formation.js';
 
 export class GameMatch {
  constructor(options={}){
@@ -21,21 +23,30 @@ export class GameMatch {
 
   this.controller=new MatchController(this.world);
   this.possession=new PossessionManager();
-  this.coach={
-   home:new CoachAI(this.engine.home),
-   away:new CoachAI(this.engine.away)
-  };
+  this.coach={home:new CoachAI(this.engine.home),away:new CoachAI(this.engine.away)};
   this.stamina=new StaminaSystem();
   this.referee=new Referee();
   this.ballAI=new BallAI(this.world.ball);
+  this.positioning=new PositioningEngine();
 
+  this.setupFormation(options.formation || '4-4-2');
   this.controller.initialize();
   this.running=false;
  }
 
+ setupFormation(type){
+  const roles=createPositions(type);
+  this.players.forEach((player,index)=>{
+   const role=roles[index%roles.length];
+   player.role=role;
+   const pos=this.positioning.getPosition(role,player.team);
+   player.x=pos.x;
+   player.y=pos.y;
+  });
+ }
+
  frame(delta=1){
   if(!this.running) return this.state;
-
   this.updateAI(delta);
   this.updateStamina();
   this.updatePlayers(delta);
@@ -43,47 +54,23 @@ export class GameMatch {
   this.checkPossession();
   this.updateCoaches();
   this.updateMatchTime();
-
   return this.state;
  }
 
- updatePlayers(){
-  this.world.update();
- }
-
- updateAI(){
-  this.controller.update();
- }
-
- updateBall(){
-  if(this.ballAI) this.ballAI.update();
- }
-
- updateStamina(){
-  this.players.forEach(player=>this.stamina.update(player));
- }
-
- updateCoaches(){
-  this.coach.home.analyze(this.state);
-  this.coach.away.analyze(this.state);
- }
+ updatePlayers(){this.world.update();}
+ updateAI(){this.controller.update();}
+ updateBall(){if(this.ballAI)this.ballAI.update();}
+ updateStamina(){this.players.forEach(p=>this.stamina.update(p));}
+ updateCoaches(){this.coach.home.analyze(this.state);this.coach.away.analyze(this.state);}
 
  checkPossession(){
   const owner=this.possession.update(this.world.players,this.world.ball);
   if(owner){
-   this.state.possession={
-    home:owner.team==='home'?100:0,
-    away:owner.team==='away'?100:0
-   };
+   this.state.possession={home:owner.team==='home'?100:0,away:owner.team==='away'?100:0};
   }
  }
 
- updateMatchTime(){
-  if(this.state.minute!==this.engine.minute){
-   this.state.minute=this.engine.minute;
-  }
- }
-
+ updateMatchTime(){if(this.state.minute!==this.engine.minute)this.state.minute=this.engine.minute;}
  start(){this.running=true;}
  stop(){this.running=false;}
  getScore(){return this.state.score;}
