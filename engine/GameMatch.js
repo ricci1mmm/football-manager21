63854724be9384111a27fb13740d21efe61e37b9
ricci1mmm <76752981+ricci1mmm@users.noreply.github.com>
2026-07-28 00:10:47 +0300

    Integrate tactical AI into match loop

--- engine/GameMatch.js
+++ engine/GameMatch.js
@@ -9,13 +9,13 @@ import {Referee} from './referee.js';
 import {BallAI} from './ballAI.js';
 import {PositioningEngine} from './positioning.js';
 import {createPositions} from './formation.js';
+import {TacticalAI} from './tacticalAI.js';
 
 export class GameMatch {
  constructor(options={}){
   this.engine=options.engine || new MatchEngine();
   this.world=options.world || new GameWorld();
   this.state=options.state || new MatchState();
-
   this.players=[...(this.engine.home || []), ...(this.engine.away || [])];
 
   this.world.addPlayers(this.players);
@@ -24,6 +24,7 @@ export class GameMatch {
   this.controller=new MatchController(this.world);
   this.possession=new PossessionManager();
   this.coach={home:new CoachAI(this.engine.home),away:new CoachAI(this.engine.away)};
+  this.tactics={home:new TacticalAI(this.engine.home),away:new TacticalAI(this.engine.away)};
   this.stamina=new StaminaSystem();
   this.referee=new Referee();
   this.ballAI=new BallAI(this.world.ball);
@@ -48,6 +49,7 @@ export class GameMatch {
  frame(delta=1){
   if(!this.running) return this.state;
   this.updateAI(delta);
+  this.updateTactics();
   this.updateStamina();
   this.updatePlayers(delta);
   this.updateBall(delta);
@@ -57,19 +59,19 @@ export class GameMatch {
   return this.state;
  }
 
- updatePlayers(){this.world.update();}
  updateAI(){this.controller.update();}
+ updateTactics(){
+  this.tactics.home.update(this.state);
+  this.tactics.away.update(this.state);
+ }
+ updatePlayers(){this.world.update();}
  updateBall(){if(this.ballAI)this.ballAI.update();}
  updateStamina(){this.players.forEach(p=>this.stamina.update(p));}
  updateCoaches(){this.coach.home.analyze(this.state);this.coach.away.analyze(this.state);}
-
  checkPossession(){
   const owner=this.possession.update(this.world.players,this.world.ball);
-  if(owner){
-   this.state.possession={home:owner.team==='home'?100:0,away:owner.team==='away'?100:0};
-  }
+  if(owner)this.state.possession={home:owner.team==='home'?100:0,away:owner.team==='away'?100:0};
  }
-
  updateMatchTime(){if(this.state.minute!==this.engine.minute)this.state.minute=this.engine.minute;}
  start(){this.running=true;}
  stop(){this.running=false;}
