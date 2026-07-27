33d440818365c78478434ff178b49b3c1d9c16ed
ricci1mmm <76752981+ricci1mmm@users.noreply.github.com>
2026-07-28 00:09:46 +0300

    Integrate coach, stamina, referee and ball systems into GameMatch

--- engine/GameMatch.js
+++ engine/GameMatch.js
@@ -3,36 +3,45 @@ import {MatchState} from './matchState.js';
 import {MatchEngine} from './matchEngine.js';
 import {MatchController} from './matchController.js';
 import {PossessionManager} from './possession.js';
+import {CoachAI} from './coachAI.js';
+import {StaminaSystem} from './stamina.js';
+import {Referee} from './referee.js';
+import {BallAI} from './ballAI.js';
 
 export class GameMatch {
  constructor(options={}){
-  this.engine = options.engine || new MatchEngine();
-  this.world = options.world || new GameWorld();
-  this.state = options.state || new MatchState();
+  this.engine=options.engine || new MatchEngine();
+  this.world=options.world || new GameWorld();
+  this.state=options.state || new MatchState();
 
-  this.players = [
-   ...(this.engine.home || []),
-   ...(this.engine.away || [])
-  ];
+  this.players=[...(this.engine.home || []), ...(this.engine.away || [])];
 
   this.world.addPlayers(this.players);
   this.world.setBall(this.engine.ball);
 
-  this.controller = new MatchController(this.world);
-  this.possession = new PossessionManager();
+  this.controller=new MatchController(this.world);
+  this.possession=new PossessionManager();
+  this.coach={
+   home:new CoachAI(this.engine.home),
+   away:new CoachAI(this.engine.away)
+  };
+  this.stamina=new StaminaSystem();
+  this.referee=new Referee();
+  this.ballAI=new BallAI(this.world.ball);
 
   this.controller.initialize();
-
-  this.running = false;
+  this.running=false;
  }
 
  frame(delta=1){
   if(!this.running) return this.state;
 
   this.updateAI(delta);
+  this.updateStamina();
   this.updatePlayers(delta);
   this.updateBall(delta);
   this.checkPossession();
+  this.updateCoaches();
   this.updateMatchTime();
 
   return this.state;
@@ -47,40 +56,35 @@ export class GameMatch {
  }
 
  updateBall(){
-  if(this.world.ball && this.world.ball.update){
-   this.world.ball.update();
-  }
+  if(this.ballAI) this.ballAI.update();
+ }
+
+ updateStamina(){
+  this.players.forEach(player=>this.stamina.update(player));
+ }
+
+ updateCoaches(){
+  this.coach.home.analyze(this.state);
+  this.coach.away.analyze(this.state);
  }
 
  checkPossession(){
-  const owner=this.possession.update(
-   this.world.players,
-   this.world.ball
-  );
-
+  const owner=this.possession.update(this.world.players,this.world.ball);
   if(owner){
    this.state.possession={
-    home: owner.team==='home'?100:0,
-    away: owner.team==='away'?100:0
+    home:owner.team==='home'?100:0,
+    away:owner.team==='away'?100:0
    };
   }
  }
 
  updateMatchTime(){
-  if(this.state.minute !== this.engine.minute){
+  if(this.state.minute!==this.engine.minute){
    this.state.minute=this.engine.minute;
   }
  }
 
- start(){
-  this.running=true;
- }
-
- stop(){
-  this.running=false;
- }
-
- getScore(){
-  return this.state.score;
- }
+ start(){this.running=true;}
+ stop(){this.running=false;}
+ getScore(){return this.state.score;}
 }
